b333d0907d8e1e1fd257ee0f3bc6143d20e1ff57
deidara047 <deidarachamp@gmail.com>
2024-07-15 23:39:48 -0600

    Fixing params error
    Basically adding the params param to performAction type

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
+    transpilePackages: ['@ts-jison/parser-generator']
   }
 };
 
