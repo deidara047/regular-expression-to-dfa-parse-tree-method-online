7fa15a50d52d4313983f198f23d87a0a513a1934
deidara047 <deidarachamp@gmail.com>
2024-07-15 23:46:27 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
-    transpilePackages: ['@ts-jison/parser-generator']
-  }
+    ignoreBuildErrors: true
+  },
+  transpilePackages: ['@ts-jison/parser']
 };
 
 export default nextConfig;
